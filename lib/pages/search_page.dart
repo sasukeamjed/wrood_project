7b7451a8a1f2e58a3ee64eab2daa8c08803d3997
sasukeamjed <sasukeamjed@gmail.com>
2019-04-01 04:39:09 +0400

    adding ui to search page

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -3,8 +3,23 @@ import 'package:flutter/material.dart';
 class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Search Page'),
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Material(
+            elevation: 5,
+            borderRadius: BorderRadius.circular(10),
+            child: TextField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search),
+                hintText: 'Search',
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
